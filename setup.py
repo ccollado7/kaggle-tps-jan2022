1ff023f04fc4380cfc68b0f9f0630ee0217c98c6
ClaudioC <cjccollado@gmail.com>
2022-01-02 16:02:19 -0300

    Creacion del repositorio

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repositorio para la competencia de Kaggle Tabular Playground Series - Jan 2022',
+    author='Claudio Collado',
+    license='MIT',
+)
